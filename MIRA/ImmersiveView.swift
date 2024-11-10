b35d944d2af80e4c9782e80bda028986f4a47749
FeoluK <olufeolu@gmail.com>
2024-11-09 17:20:11 -0800

    V2 Commit

--- MIRA/ImmersiveView.swift
+++ MIRA/ImmersiveView.swift
@@ -20,7 +20,7 @@ struct ImmersiveView: View {
                 angelEntity.scale = SIMD3<Float>(0.1, 0.1, 0.1) // Adjust scale as needed
                 
                 // Move the model to a new position
-                angelEntity.position = SIMD3<Float>(x: 0.0, y: -0.5, z: -1.0) // Adjust position as needed
+                angelEntity.position = SIMD3<Float>(x: 75, y: 20, z: -150) // Adjust position as needed
                 
                 if let animation = angelEntity.availableAnimations.first {
                     angelEntity.playAnimation(animation.repeat())
@@ -31,10 +31,10 @@ struct ImmersiveView: View {
                 content.add(devilEntity)
                 
                 // Scale down the model
-                devilEntity.scale = SIMD3<Float>(0.1, 0.1, 0.1) // Adjust scale as needed
+                devilEntity.scale = SIMD3<Float>(0.2, 0.2, 0.2) // Adjust scale as needed
                 
                 // Move the model to a new position
-                devilEntity.position = SIMD3<Float>(x: 0.0, y: -0.5, z: -125.0) // Adjust position as needed
+                devilEntity.position = SIMD3<Float>(x: -75.0, y: 20, z: -150.0) // Adjust position as needed
                 
                 if let animation = devilEntity.availableAnimations.first {
                     devilEntity.playAnimation(animation.repeat())
